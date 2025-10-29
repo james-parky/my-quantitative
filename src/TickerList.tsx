37aeae2661ca54e75b17e9bdd9fc081c5d68fb77
Jason Ho <jasonho25801@gmail.com>
2025-10-29 23:58:27 +0000

    Add TradeForm component, live random pricing

--- src/TickerList.tsx
+++ src/TickerList.tsx
@@ -1,53 +1,70 @@
+import { useMemo } from "react";
+import type { PositionData } from "./App.tsx";
 import Ticker from "./Ticker.tsx";
 
 export type TickerData = {
-    shortName: string,
-    longName: string,
-    currency: Currency,
-}
+  shortName: string;
+  longName: string;
+  currency: Currency;
+  points: number[];
+};
 
 type Currency = "US$" | "GB£";
-type Color = string;
-type Trend = "up" | "down";
 
-export default function TickerList({tickerData}: { tickerData: TickerData[] }) {
-    const positivePriceChangeColor: Color = "#61FB6B";
-    const negativePriceChangeColor: Color = "#EE4f4D";
-
-    return <div className="ticker-list">
-        {tickerData.map((item, i) => {
-            // TODO: points are randomly generated here but will be supplied as
-            //       a field of TickerData from real data.
-            const upper = 50;
-            const points = nRandomPointsInRange(upper, 260, 130, "up");
-            const priceChange = points[points.length - 1] - points[0];
-            const color = priceChange > 0 ? positivePriceChangeColor : negativePriceChangeColor;
-
-            return <Ticker key={i}>
-                <Ticker.Names long={item.longName} short={item.shortName}/>
-                <Ticker.Graph color={color}
-                              upper={upper} points={points} boundingRect={{length: 800, width: 300}}/>
-                <Ticker.Prices current={points[points.length - 1]}
-                               change={priceChange}
-                               currency={item.currency}
-                               color={color}/>
-            </Ticker>
-        })}
-    </div>
+interface TickerListProps {
+  instruments: Map<string, TickerData>;
+  positions: Map<string, PositionData>;
+  prices: Map<string, number[]>;
 }
 
-const nRandomPointsInRange = (n: number, min: number, max: number, trend: Trend): number[] => {
-    const values = [];
-    let current = Math.floor((min + max) / 2);
-    for (let i = 0; i < n; i++) {
-        const range = max - min;
-        const jump = Math.random() < 0.5 ? Math.floor(Math.random() * (range * 0.1)) + 5 : Math.floor(Math.random() * (range * 0.3)) + 15; // TODO: bruh
-        const bias = trend == "up" ? 0.8 : -0.04;
-        const dir = Math.random() + bias > 0.5 ? 1 : -1;
-        current += dir * jump;
-        values.push(current);
+export default function TickerList({
+  instruments,
+  positions,
+  prices,
+}: TickerListProps) {
+  const positivePriceChangeColor = "#61FB6B";
+  const negativePriceChangeColor = "#EE4f4D";
+  const tickersWithPositions = useMemo(() => {
+    if (!instruments || !positions) {
+      return [];
     }
+    const temp: TickerData[] = [];
+    Array.from(positions.keys()).forEach((key) => {
+      const item = instruments.get(key);
+      if (item) {
+        temp.push(item);
+      }
+    });
+    return temp;
+  }, [instruments, positions]);
 
-    return values;
+  return (
+    <div className="ticker-list">
+      {tickersWithPositions.map((item) => {
+        const upper = 50;
+        const points = prices.get(item.shortName) || [];
+        const priceChange = points[points.length - 1] - points[0];
+        const color =
+          priceChange > 0 ? positivePriceChangeColor : negativePriceChangeColor;
 
+        return (
+          <Ticker key={item.shortName}>
+            <Ticker.Names long={item.longName} short={item.shortName} />
+            <Ticker.Graph
+              color={color}
+              upper={upper}
+              points={points}
+              boundingRect={{ length: 800, width: 300 }}
+            />
+            <Ticker.Prices
+              current={points[points.length - 1]}
+              change={priceChange}
+              currency={item.currency}
+              color={color}
+            />
+          </Ticker>
+        );
+      })}
+    </div>
+  );
 }
